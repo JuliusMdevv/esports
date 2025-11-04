aeab4168f15d6d214ad37258a94badaac4354bda
Julius Markwick <julius.markwick@gmail.com>
2025-11-04 10:21:04 -0800

    idk you can finally navogate

--- js/dropdown.js
+++
@@ -1,8 +0,0 @@
-const dropGames = document.querySelector('.dropGamesButton');
-
-const dropGamesMenu = document.querySelector('.dropGamesContent');
-
-dropGames.addEventListener('click', () => {
-    dropGames.classList.toggle('active');
-    dropGamesMenu.classList.toggle('active');
-})
